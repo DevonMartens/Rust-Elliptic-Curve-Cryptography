5e902713e415fdc632e9c97b414e93763547e3a9
Devon <dmmartens91@gmail.com>
2024-02-19 15:49:07 +0100

    saves progress on EC points

--- EC-Cryptography/src/lib.rs
+++ EC-Cryptography/src/lib.rs
@@ -5,9 +5,9 @@ use num_bigint::BigUint;
 
 
 // Point on the elliptic curve
-struct Point {
-    x: BigUint,
-    y: BigUint,
+enum Point {
+    Coordinate(BigUint, BigUint),
+    Identity,
 }
 struct EllipticCurve {
     // y^2 = x^3 + ax + b
@@ -19,8 +19,40 @@ struct EllipticCurve {
 // implement addition 
 impl EllipticCurve {
   //  passing points by reference
-    fn add(c: &Point, d: &Point) -> Point {
-        todo!("Implement the add function");
+    fn add(&self, c: &Point, d: &Point) -> Point {
+        assert!(
+            self.is_on_curve(c),
+            "The first point is not on the curve"
+        );
+        assert!(
+            self.is_on_curve(d),
+            "The second point is not on the curve"
+        );
+        match (c,d) => {
+            (Point::Identity, _) => d,
+            (_, Point::Identity) => c,
+            (Point::Coordinate(x1, y1), Point::Coordinate(x2, y2)) => {
+                // s= (y2-y1)/(x2-x1) mod p
+                // x3 = s^2 - x1 - x2 mod p
+                // y3 = s(x1-x3) - y1  mod p
+                let s: () =
+                if x1 == x2 && y1 != y2 {
+                    return Point::Identity;
+                }
+                if x1 == x2 && y1 == y2 {
+                    return self.double(c);
+                }
+                let lambda = if x1 == x2 {
+                    (BigUint::from(3u32) * x1.pow(BigUint::from(2u32)) + &self.a) * FiniteField::inverse_multiplication(&BigUint::from(2u32) * y1, &self.p)
+                } else {
+                    (y2 - y1) * FiniteField::inverse_multiplication(&(x2 - x1), &self.p)
+                };
+                let x3 = lambda.pow(BigUint::from(2u32)) - x1 - x2;
+                let y3 = lambda * (x1 - x3) - y1;
+                return Point::Coordinate(x3, y3);
+            }
+        }
+
     }
 
     fn double(c: &Point){
@@ -28,6 +60,19 @@ impl EllipticCurve {
     }
     fn scalar_mul(c: &Point, n: BigUint) -> Point {
         todo!("Implement the multiply function")
+    }
+    fn is_on_curve(c: &Point) -> bool {
+        // y^2 = x^3 + ax + b
+        let y2 = match c {
+           Point::Coordinate(x, y) => {
+            let y2 = y.modpow(&BigUint::from(2u32), &this.p)
+            let x3 = x.modpow(&BigUint::from(3u32), &this.p)
+            let ax = &self.a * x
+            y2 == x3 + ax + &self.b;
+           }
+            Point::Identity => return false,
+        };
+        todo!("Implement the is_on_curve function")
     }
 }
 
@@ -53,10 +98,17 @@ impl FiniteField {
         return r % p;
         
     }
+    pub fn inverse_addition(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint {
+        let d_inv = FiniteField::inv_addition(d, p);
+    }
     pub fn inverse_multiplication(c: &BigUint, p:&BigUint) -> BigUint {
         let r = c.modpow(&(p - BigUint::from(2u32)), p);
         return r;
 }
+    pub fn subtract(c: &BigUint, d: &BigUint, p: &BigUint) -> BigUint {
+        let r = c - d;
+        return r % p;
+    }
 }
 
 
@@ -123,4 +175,6 @@ mod tests {
         let r = FiniteField::inverse_multiplication(&c, &p);
         assert_eq!(r, BigUint::from(3u32));
     }
+    #[test]
+
 }
